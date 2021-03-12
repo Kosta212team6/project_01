f268db516d644e3152b8acf677ddd4b1aa9bbbb3
Subin <72715023+lightsaber29@users.noreply.github.com>
2021-03-12 16:52:41 +0900

    숩_Member Rent Return DTO 내용 추가

--- src/kosta/mvc/model/dto/MemberDTO.java
+++ src/kosta/mvc/model/dto/MemberDTO.java
@@ -1,5 +1,71 @@
 package kosta.mvc.model.dto;
 
 public class MemberDTO {
+	private String mID;
+	private String mName;
+	private String mPhone;
+	private String mPwd;
+	private String mAble;
+	private int mStatus;
+	private int nCode;
+	
+	public MemberDTO() {}
+	public MemberDTO(String mID, String mName, String mPhone, String mPwd, String mAble, int mStatus, int nCode) {
+		super();
+		this.mID = mID;
+		this.mName = mName;
+		this.mPhone = mPhone;
+		this.mPwd = mPwd;
+		this.mAble = mAble;
+		this.mStatus = mStatus;
+		this.nCode = nCode;
+	}
+	public String getmID() {
+		return mID;
+	}
+	public void setmID(String mID) {
+		this.mID = mID;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public String getmPhone() {
+		return mPhone;
+	}
+	public void setmPhone(String mPhone) {
+		this.mPhone = mPhone;
+	}
+	public String getmPwd() {
+		return mPwd;
+	}
+	public void setmPwd(String mPwd) {
+		this.mPwd = mPwd;
+	}
+	public String getmAble() {
+		return mAble;
+	}
+	public void setmAble(String mAble) {
+		this.mAble = mAble;
+	}
+	public int getmStatus() {
+		return mStatus;
+	}
+	public void setmStatus(int mStatus) {
+		this.mStatus = mStatus;
+	}
+	public int getnCode() {
+		return nCode;
+	}
+	public void setnCode(int nCode) {
+		this.nCode = nCode;
+	}
+	@Override
+	public String toString() {
+		return mID + " | " + mName + " | " + mPhone + " | " + mPwd + " | " + mAble
+				+ " | " + mStatus + " | " + nCode;
+	}
 
 }
--- src/kosta/mvc/model/dto/ReturnDTO.java
+++ src/kosta/mvc/model/dto/ReturnDTO.java
@@ -1,5 +1,38 @@
 package kosta.mvc.model.dto;
 
 public class ReturnDTO {
-
+	private int rNum;
+	private String rtDate;
+	private int bISBN;
+	
+	public ReturnDTO() {}
+	public ReturnDTO(int rNum, String rtDate, int bISBN) {
+		super();
+		this.rNum = rNum;
+		this.rtDate = rtDate;
+		this.bISBN = bISBN;
+	}
+	public int getrNum() {
+		return rNum;
+	}
+	public void setrNum(int rNum) {
+		this.rNum = rNum;
+	}
+	public String getRtDate() {
+		return rtDate;
+	}
+	public void setRtDate(String rtDate) {
+		this.rtDate = rtDate;
+	}
+	public int getbISBN() {
+		return bISBN;
+	}
+	public void setbISBN(int bISBN) {
+		this.bISBN = bISBN;
+	}
+	@Override
+	public String toString() {
+		return rNum + " | " + rtDate + " | " + bISBN;
+	}
+	
 }
