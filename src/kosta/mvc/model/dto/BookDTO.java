f664a2119aa3605b2d7d585c877284412951c7aa
Lee SeungHyun <Lee SeungHyun@DESKTOP-52A4FL1>
2021-03-12 16:53:44 +0900

    승_bookDTO, RsvDTO, SortDTO committed

--- src/kosta/mvc/model/dto/BookDTO.java
+++ src/kosta/mvc/model/dto/BookDTO.java
@@ -1,5 +1,86 @@
 package kosta.mvc.model.dto;
 
 public class BookDTO {
+	private int bIsbn; //pk
+	private String bName;
+	private String bWrite;
+	private String bPub;
+	private String bDate;
+	private int bStatus;
+	private int sCode; //fk
+	
+	public BookDTO() {}
+	
+	public BookDTO(int bIsbn, String bName, String bWrite, String bPub, String bDate, int bStatus, int sCode) {
+		super();
+		this.bIsbn = bIsbn;
+		this.bName = bName;
+		this.bWrite = bWrite;
+		this.bPub = bPub;
+		this.bDate = bDate;
+		this.bStatus = bStatus;
+		this.sCode = sCode;
+	}
 
+	public int getbIsbn() {
+		return bIsbn;
+	}
+
+	public void setbIsbn(int bIsbn) {
+		this.bIsbn = bIsbn;
+	}
+
+	public String getbName() {
+		return bName;
+	}
+
+	public void setbName(String bName) {
+		this.bName = bName;
+	}
+
+	public String getbWrite() {
+		return bWrite;
+	}
+
+	public void setbWrite(String bWrite) {
+		this.bWrite = bWrite;
+	}
+
+	public String getbPub() {
+		return bPub;
+	}
+
+	public void setbPub(String bPub) {
+		this.bPub = bPub;
+	}
+
+	public String getbDate() {
+		return bDate;
+	}
+
+	public void setbDate(String bDate) {
+		this.bDate = bDate;
+	}
+
+	public int getbStatus() {
+		return bStatus;
+	}
+
+	public void setbStatus(int bStatus) {
+		this.bStatus = bStatus;
+	}
+
+	public int getsCode() {
+		return sCode;
+	}
+
+	public void setsCode(int sCode) {
+		this.sCode = sCode;
+	}
+	
+	@Override
+	public String toString() {
+		return bIsbn + " | " + bName + " | " +  bWrite + " | " +  bPub + " | " +  bDate + " | " +  bStatus + " | " +  sCode;
+	}
+	
 }
--- src/kosta/mvc/model/dto/RsvDTO.java
+++ src/kosta/mvc/model/dto/RsvDTO.java
@@ -1,5 +1,56 @@
 package kosta.mvc.model.dto;
 
 public class RsvDTO {
+	private int rsvNum; //pk
+	private String rsvDate;
+	private String mID; //fk
+	private int bIsbn; //fk
+	
 	public RsvDTO () {}
+
+	public RsvDTO(int rsvNum, String rsvDate, String mID, int bIsbn) {
+		super();
+		this.rsvNum = rsvNum;
+		this.rsvDate = rsvDate;
+		this.mID = mID;
+		this.bIsbn = bIsbn;
+	}
+
+	public int getRsvNum() {
+		return rsvNum;
+	}
+
+	public void setRsvNum(int rsvNum) {
+		this.rsvNum = rsvNum;
+	}
+
+	public String getRsvDate() {
+		return rsvDate;
+	}
+
+	public void setRsvDate(String rsvDate) {
+		this.rsvDate = rsvDate;
+	}
+
+	public String getmID() {
+		return mID;
+	}
+
+	public void setMID(String mID) {
+		this.mID = mID;
+	}
+
+	public int getbIsbn() {
+		return bIsbn;
+	}
+
+	public void setbIsbn(int bIsbn) {
+		this.bIsbn = bIsbn;
+	}
+	
+	@Override
+	public String toString() {
+		return rsvNum + " | " + rsvDate + " | " + mID + " | " + bIsbn;
+	}
+	
 }
